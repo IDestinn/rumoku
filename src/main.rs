ead33903138d29dc62abadf03c0b996b145be0a4
Destinn <donats2004@gmail.com>
2025-02-06 09:23:48 +0300

    Update dependencies and enhance board component with cell rendering

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
 use yew::prelude::*;
 
+#[derive(Clone, PartialEq)]
+enum CellStatus {
+    Empty,
+    _Black,
+    _White,
+}
+
+#[derive(Properties, PartialEq)]
+struct BoardProps {
+    rows: usize,
+    columns: usize,
+}
+
+#[function_component(Board)]
+fn board(BoardProps { rows, columns }: &BoardProps) -> Html {
+    let cells = vec![CellStatus::Empty; columns * rows];
+
+    html!(<div class="board">
+        {cells
+        .iter().enumerate()
+        .map(|(idx, _)| {
+            html!(<div key={idx} class="cell"></div>)
+        })
+        .collect::<Html>()}
+    </div>)
+}
+
 #[function_component(App)]
 pub fn app() -> Html {
+    const COLUMNS: usize = 15;
+    const ROWS: usize = 15;
     html! {
         <main>
-            <div class="board">
-            </div>
+            <Board rows={ROWS} columns={COLUMNS}/>
         </main>
     }
 }
